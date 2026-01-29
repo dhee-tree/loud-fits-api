39b3111278c8d73cb752c6b7f815bdbbd971962f
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2026-01-29 02:43:43 +0000

    Improve user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'username', 'first_name',
+                    'last_name',  'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+
+
+admin.site.register(User, UserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,5 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
+
 
 class User(AbstractUser):
+    uuid = models.UUIDField(primary_key=True, unique=True,
+                            editable=False, null=False, default=uuid.uuid4)
     google_id = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_google_id(self):
+        return self.google_id
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            self.uuid = uuid.uuid4()
+        super().save(*args, **kwargs)
